d4e1d11b94b7794a963db809577fece1292a5431
Jeyhun Mammadsaidov <mcmemmedd@gmail.com>
2023-03-13 22:31:31 +0400

    create entity user and role

--- common-security/src/main/java/az/cmammad/security/jwt/JwtService.java
+++ common-security/src/main/java/az/cmammad/security/jwt/JwtService.java
@@ -1,5 +1,6 @@
 package az.cmammad.security.jwt;
 
+import az.cmammad.common.entity.UserEntity;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
@@ -7,10 +8,12 @@ import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
@@ -20,7 +23,17 @@ public class JwtService {
 
     private final HttpServletRequest httpServletRequest;
 
-   /*burada bezi case'leri override ele, hard coded qalmasin*/
+    public String generateAccessToken(final UserEntity user) {
+        final Map<String, String> claimsMap = new HashMap<>();
+        claimsMap.put("username", user.getUsername());
+        claimsMap.put("full_name", user.getFullName());
+        claimsMap.put("address", user.getAddress());
+        claimsMap.put("user_status", user.getUserStatus().name());
+        claimsMap.put("create_date", user.getCreatedDate().toString());
+        return createToken(claimsMap, user.getUsername());
+    }
+
+    /*burada bezi case'leri override ele, hard coded qalmasin*/
     private String createToken(final Map<String, String> claimsMap,
                                final String subject) {
         return Jwts
@@ -33,6 +46,17 @@ public class JwtService {
                 .setAudience(httpServletRequest.getRemoteHost())
                 .setIssuer("Jeyhun Mammadsaidov")
                 .compact();
+    }
+
+    public boolean validToken(final String token,
+                              final UserDetails userDetails) {
+        final var username = extractUsername(token);
+        return (username.equals(userDetails.getUsername()) && !isExpireToken(token));
+    }
+
+    private boolean isExpireToken(final String token) {
+        final var expireToken = extractClaim(token, Claims::getExpiration);
+        return expireToken.before(new Date());
     }
 
     public String extractUsername(final String token) {
---
+++ common/src/main/java/az/cmammad/common/entity/RoleEntity.java
@@ -0,0 +1,36 @@
+package az.cmammad.common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@Builder
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = RoleEntity.TABLE_NAME)
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class RoleEntity {
+
+    public static final String TABLE_NAME = "user_roles";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @Column(
+            name = "name",
+            nullable = false
+    )
+    String name;
+}
