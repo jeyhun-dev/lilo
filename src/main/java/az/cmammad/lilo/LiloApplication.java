a97f0c53693e8d9f5de4e4bd5cad5d9c4d2eba4a
Jeyhun Mammadsaidov <mcmemmedd@gmail.com>
2023-03-13 20:59:08 +0400

    initial commit

---
+++ src/main/java/az/cmammad/lilo/LiloApplication.java
@@ -0,0 +1,13 @@
+package az.cmammad.lilo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiloApplication.class, args);
+	}
+
+}
